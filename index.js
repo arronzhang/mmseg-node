d30af5d0557d3f24c9aebb9d23afa2499b2544c5
Hidden <zzdhidden@gmail.com>
2011-06-01 19:40:23 +0800

    Add clean method for clean symbol

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ var mmseg = require("./build/default/mmseg.node").mmseg;
 module.exports.open =  function(file) {
 	return new mmseg(file);
 };
+
+/*
+*
+* Clean
+*
+* $param array ar
+* $return array ar. Only return number, letter, chinese word
+*
+*/
+module.exports.clean =function(ar) {
+	if( ar && ar.length ){
+		var a = [];
+		for (var i = 0, l = ar.length; i < l; i++) {
+			if( /^[\u4e00-\u9fa5\da-zA-Z]+$/.test(ar[i]) ) a.push(ar[i]);
+		};
+		ar = a;
+	}
+	return ar;
+};
