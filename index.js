d241fcc92ef032d53430e407a2d86b9464dd7ad4
simsicon <simsicon@gmail.com>
2012-01-06 12:02:08 +0800

    fix path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mmseg = require("./build/default/mmseg.node").mmseg;
+var mmseg = require("./build/Release/mmseg.node").mmseg;
 
 module.exports.open =  function(file) {
 	return new mmseg(file);
